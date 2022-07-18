802cbb9e16c11ba1c01c90bbc47542a28a3a95c4
Alexey <koles4854@gmail.com>
2022-07-18 16:29:31 +0300

    add finished the content

--- src/components/ConverterField.js
+++ src/components/ConverterField.js
@@ -12,52 +12,71 @@ const ConverterField = () => {
 
     return (
         <Box>
+        <Box sx={{ margin : 25,padding: 1, display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignContent: 'space-between' }}>
+            <Box>
+                <Typography>У меня есть</Typography>
+                <Box sx={{ width: 400, height: 200, border: 1, borderColor: 'grey.500', borderRadius: 2 }}>
+                    <FormControl variant="standard" fullWidth>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Age"
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
 
-            <Typography>У меня есть</Typography>
+                    <TextField sx={{ mt: 5.5, ml: 2 }}
+                        id="standard-number"
+                        label="Number"
+                        type="number"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        variant="standard"
+                    />
 
-
-            <Box sx={{ width: 400, height: 200, border: 1, borderColor: 'grey.500', borderRadius: 2 }}>
-
-                <FormControl variant="standard" fullWidth>
-
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-
-                        label="Age"
-
-                    >
-                        <MenuItem value={10}>Ten</MenuItem>
-                        <MenuItem value={20}>Twenty</MenuItem>
-                        <MenuItem value={30}>Thirty</MenuItem>
-                    </Select>
-                </FormControl>
-
-
-                <TextField sx={{ mt: 5.5, ml: 2 }}
-                    id="standard-number"
-                    label="Number"
-                    type="number"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    variant="standard"
-                />
-
-
-                <Typography sx={{ mt: 5, ml: 2 }}>1110р</Typography>
+                    <Typography sx={{ mt: 5, ml: 2 }}>1110р</Typography>
+                </Box>
             </Box>
 
-            <Box><Button ><CompareArrowsIcon sx= {{width: 50, height: 50}} /></Button></Box>
+            <Box sx={{ display: "flex", padding: 1, alignItems: 'center', }}><Button   ><CompareArrowsIcon sx={{ width: 50, height: 50, }} /></Button></Box>
+
+            <Box>
+                <Typography>У меня есть</Typography>
+                <Box sx={{ width: 400, height: 200, border: 1, borderColor: 'grey.500', borderRadius: 2 }}>
+                    <FormControl variant="standard" fullWidth>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Age"
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+
+                    <TextField sx={{ mt: 5.5, ml: 2 }}
+                        id="standard-number"
+                        label="Number"
+                        type="number"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        variant="standard"
+                    />
+
+                    <Typography sx={{ mt: 5, ml: 2 }}>1110р</Typography>
+                </Box>
+            </Box>
+
+
         </Box>
 
-
-
-
-
-
-
-
+        </Box>
     )
 }
 
