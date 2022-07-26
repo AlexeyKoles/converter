d19ee9006a6d73c5864daad6a00f96fcb616bd77
Alexey <koles4854@gmail.com>
2022-07-26 11:31:15 +0300

    a working list was compiled

--- src/components/ConverterField.js
+++ src/components/ConverterField.js
@@ -1,43 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-
+import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
-import NativeSelect from '@mui/material/NativeSelect';
-
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
 
 
 const ConverterField = () => {
 
     const [data1, setData1] = useState([]);
-    const [data2, setData2] = useState(null);
-    const [ratesValue, setRatesValue] = useState(0);
-    const [ratesValue2, setRatesValue2] = useState(0);
-    const [text, setText] = useState(0);
-    const [text2, setText2] = useState(0);
+    const [data2, setData2] = useState([]);
+    const [rat1, setRat1] = useState();
+    const [rat2, setRat2] = useState();
 
     useEffect(() => { fetchData() }, []);
     async function fetchData() {
         let response = await fetch("https://cdn.cur.su/api/latest.json")
         let currencyObject = await response.json();
-        setData1(currencyObject)
-        setData2(currencyObject)
-
+        setData1(currencyObject.rates)
+        setData2(currencyObject.rates)
     };
 
-    function convert(e) {
-        e.preventDefault()
-
-        let num = (ratesValue2 / ratesValue) * text;
-        setText2(num)
-    }
-    
-        
-     
-
+    const handleChange1 = (event) => {
+        setRat1(event.target.value);
+    };
+    const handleChange2 = (event) => {
+        setRat2(event.target.value);
+    };
     return (
         <Box>
             <form  >
@@ -47,22 +40,24 @@ const ConverterField = () => {
 
                         <Box sx={{ width: 400, height: 200, border: 1, borderColor: 'grey.500', borderRadius: 2 }}>
 
-                            <FormControl variant="standard"
-                                fullWidth
-                                onChange={(e) => setRatesValue(e.target.value)}>
-                                <NativeSelect>
-                                    {Object.keys(data1.rates).map((value, index) => { return (<option key={index} value={data1.rates[value]}> {value} </option>) })};
-                                </NativeSelect>
+                            <FormControl fullWidth variant="standard" >
+                                <InputLabel id="demo-simple-select-label">Выберите валюту</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={rat1}
+                                    label="Age"
+                                    onChange={handleChange1}>
+
+                                    {Object.keys(data1).map((value, index) =>
+                                        <MenuItem key={index} value={value}>{value}</MenuItem>)}
+
+                                </Select>
                             </FormControl>
 
                             <TextField sx={{ mt: 5.5, ml: 2 }}
 
                                 variant="standard"
-                                value={text}
-                                onChange={(e) => setText(e.target.value)}
-                                autoComplete="off"
-                                
-
 
                             />
                             <Typography sx={{ mt: 5, ml: 2 }}>1110р</Typography>
@@ -74,25 +69,26 @@ const ConverterField = () => {
                     <Box>
                         <Typography sx={{ mb: 1 }}>Я получу</Typography>
                         <Box sx={{ width: 400, height: 200, border: 1, borderColor: 'grey.500', borderRadius: 2 }}>
-                            <FormControl
-                                variant="standard"
-                                fullWidth
-                                onChange={(e) => setRatesValue2(e.target.value)}>
+                            <FormControl fullWidth variant="standard" >
+                                <InputLabel id="demo-simple-select-label">Выберите валюту</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={rat2}
+                                    label="Age"
+                                    onChange={handleChange2}>
 
-                                <NativeSelect>
+                                    {Object.keys(data2).map((value, index) =>
+                                        <MenuItem key={index} value={value}>{value}</MenuItem>)}
 
-                                    {Object.keys(data2.rates).map((value, index) => { return (<option key={index} value={data2.rates[value]}> {value} </option>) })}
-
-                                </NativeSelect>
-
-
+                                </Select>
                             </FormControl>
+
 
                             <TextField sx={{ mt: 5.5, ml: 2 }}
 
                                 variant="standard"
-                                value={text2}
-                                onChange={(e) => setText2(e.target.value)}
+
                             />
                             <Typography sx={{ mt: 5, ml: 2 }}>1110р</Typography>
                         </Box>
